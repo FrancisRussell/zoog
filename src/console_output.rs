5505393f734124f7bfce3fa880d3055c374d66d7
Francis Russell <francis@unchartedbackwaters.co.uk>
2022-10-05 19:55:23 +0100

    Capture and replay flush operations.

--- src/console_output.rs
+++ src/console_output.rs
@@ -3,6 +3,7 @@ use std::io::{self, Stderr, Stdout, Write};
 
 use parking_lot::{Mutex, MutexGuard};
 
+#[derive(Debug)]
 pub struct Standard {
     out: Stdout,
     err: Stderr,
@@ -53,25 +54,32 @@ impl ConsoleOutput for &Standard {
     fn err(&self) -> Self::ErrStream<'_> { &self.err }
 }
 
-struct WriteInfo {
-    id: usize,
-    length: usize,
+#[derive(Copy, Clone, Debug)]
+enum StreamOperation {
+    Write(usize),
+    Flush,
 }
 
-#[derive(Default)]
+#[derive(Debug, Default)]
 struct StreamWrites {
     data: Vec<u8>,
-    writes: VecDeque<WriteInfo>,
+    operations: VecDeque<(usize, StreamOperation)>,
 }
 
 impl StreamWrites {
     fn write(&mut self, id: usize, data: &[u8]) -> Result<usize, io::Error> {
         self.data.extend(data);
-        self.writes.push_back(WriteInfo { id, length: data.len() });
+        self.operations.push_back((id, StreamOperation::Write(data.len())));
         Ok(data.len())
+    }
+
+    fn flush(&mut self, id: usize) -> Result<(), io::Error> {
+        self.operations.push_back((id, StreamOperation::Flush));
+        Ok(())
     }
 }
 
+#[derive(Debug)]
 pub struct DelayedConsoleOutput<W: ConsoleOutput> {
     inner: W,
     next_id: Mutex<usize>,
@@ -79,11 +87,13 @@ pub struct DelayedConsoleOutput<W: ConsoleOutput> {
     err: Mutex<StreamWrites>,
 }
 
+#[derive(Debug)]
 pub struct DelayedWriter<'a> {
     next_id: &'a Mutex<usize>,
     writes: &'a Mutex<StreamWrites>,
 }
 
+#[derive(Debug)]
 pub struct LockedDelayedWriter<'a> {
     next_id: &'a Mutex<usize>,
     writes: MutexGuard<'a, StreamWrites>,
@@ -101,7 +111,16 @@ impl<'a> Write for DelayedWriter<'a> {
         writes.write(id, data)
     }
 
-    fn flush(&mut self) -> Result<(), io::Error> { Ok(()) }
+    fn flush(&mut self) -> Result<(), io::Error> {
+        let id = {
+            let mut guard = self.next_id.lock();
+            let id = *guard;
+            *guard += 1;
+            id
+        };
+        let mut writes = self.writes.lock();
+        writes.flush(id)
+    }
 }
 
 impl<'a> LockableWriter for DelayedWriter<'a> {
@@ -111,7 +130,15 @@ impl<'a> LockableWriter for DelayedWriter<'a> {
 }
 
 impl<'a> Write for LockedDelayedWriter<'a> {
-    fn flush(&mut self) -> Result<(), io::Error> { Ok(()) }
+    fn flush(&mut self) -> Result<(), io::Error> {
+        let id = {
+            let mut guard = self.next_id.lock();
+            let id = *guard;
+            *guard += 1;
+            id
+        };
+        self.writes.flush(id)
+    }
 
     fn write(&mut self, data: &[u8]) -> Result<usize, io::Error> {
         let id = {
@@ -140,13 +167,8 @@ where
     pub fn new(inner: W) -> DelayedConsoleOutput<W> {
         DelayedConsoleOutput { inner, next_id: Mutex::new(0), out: Mutex::default(), err: Mutex::default() }
     }
-}
 
-impl<W> Drop for DelayedConsoleOutput<W>
-where
-    W: ConsoleOutput,
-{
-    fn drop(&mut self) {
+    fn flush_delayed_operations(&mut self) -> Result<(), io::Error> {
         let out = self.inner.out();
         let mut out = out.lock();
         let err = self.inner.err();
@@ -157,21 +179,38 @@ where
         let mut out_offset = 0;
         let mut err_offset = 0;
         loop {
-            let next_is_stdout = match (out_writes.writes.back(), err_writes.writes.back()) {
-                (Some(out_write), Some(err_write)) => out_write.id < err_write.id,
+            let next_is_stdout = match (out_writes.operations.back(), err_writes.operations.back()) {
+                (Some((out_id, _)), Some((err_id, _))) => out_id < err_id,
                 (Some(_), None) => true,
                 (None, Some(_)) => false,
                 (None, None) => break,
             };
-            let (writer, offset, data, write_info): (&mut dyn Write, _, _, _) = if next_is_stdout {
-                let write_info = out_writes.writes.pop_back().expect("Unexpectedly failed to pop write");
-                (&mut out, &mut out_offset, &out_writes.data, write_info)
+            let (writer, offset, data, op): (&mut dyn Write, _, _, _) = if next_is_stdout {
+                let (_id, op) = out_writes.operations.pop_back().expect("Unexpectedly failed to pop operation");
+                (&mut out, &mut out_offset, &out_writes.data, op)
             } else {
-                let write_info = err_writes.writes.pop_back().expect("Unexpectedly failed to pop write");
-                (&mut err, &mut err_offset, &err_writes.data, write_info)
+                let (_id, op) = err_writes.operations.pop_back().expect("Unexpectedly failed to pop operation");
+                (&mut err, &mut err_offset, &err_writes.data, op)
             };
-            writer.write_all(&data[*offset..(*offset + write_info.length)]).unwrap();
-            *offset += write_info.length;
+            match op {
+                StreamOperation::Write(length) => {
+                    writer.write_all(&data[*offset..(*offset + length)])?;
+                    *offset += length;
+                }
+                StreamOperation::Flush => {
+                    writer.flush()?;
+                }
+            }
         }
+        *out_writes = StreamWrites::default();
+        *err_writes = StreamWrites::default();
+        Ok(())
     }
+}
+
+impl<W> Drop for DelayedConsoleOutput<W>
+where
+    W: ConsoleOutput,
+{
+    fn drop(&mut self) { let _ = self.flush_delayed_operations(); }
 }
