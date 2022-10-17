2c47b2261f6d9fe9aaec0cf2b67184cad7cf8793
Francis Russell <francis@unchartedbackwaters.co.uk>
2022-10-17 21:23:04 +0100

    Implement list operation for opuscomment.

--- src/bin/opuscomment.rs
+++ src/bin/opuscomment.rs
@@ -1,6 +1,11 @@
+use std::fs::File;
+use std::io::{self, BufReader, BufWriter, Write};
 use std::path::PathBuf;
 
 use clap::Parser;
+use zoog::comment_rewriter::{CommentHeaderRewrite, CommentRewriterAction, CommentRewriterConfig};
+use zoog::header_rewriter::{rewrite_stream, SubmitResult};
+use zoog::opus::CommentList;
 use zoog::Error;
 
 fn main() {
@@ -30,6 +35,9 @@ struct Cli {
 
     /// Input file
     input_file: PathBuf,
+
+    /// Output file
+    output_file: Option<PathBuf>,
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -39,6 +47,21 @@ enum OperationMode {
     Replace,
 }
 
+#[derive(Debug)]
+enum OutputFile {
+    Temp(tempfile::NamedTempFile),
+    Sink(io::Sink),
+}
+
+impl OutputFile {
+    fn as_write(&mut self) -> &mut dyn Write {
+        match self {
+            OutputFile::Temp(ref mut temp) => temp,
+            OutputFile::Sink(ref mut sink) => sink,
+        }
+    }
+}
+
 fn main_impl() -> Result<(), Error> {
     let cli = Cli::parse_from(wild::args_os());
     let operation_mode = match (cli.list, cli.append, cli.write) {
@@ -46,10 +69,51 @@ fn main_impl() -> Result<(), Error> {
         (false, true, false) => OperationMode::Append,
         (false, false, true) => OperationMode::Replace,
         _ => {
-            //TODO: Replace me with an error
+            //FIXME: Replace me with an error
             panic!("Conflicting options supplied for mode of operation");
         }
     };
+
     println!("Operating in mode: {:?}", operation_mode);
+
+    let mut output_file = match operation_mode {
+        OperationMode::List => OutputFile::Sink(io::sink()),
+        OperationMode::Append | OperationMode::Replace => todo!("Append and replace not yet implemented"),
+    };
+    let action = match operation_mode {
+        OperationMode::List => CommentRewriterAction::NoChange,
+        OperationMode::Append | OperationMode::Replace => todo!("Append and replace not yet implemented"),
+    };
+
+    let rewriter_config = CommentRewriterConfig { action };
+
+    let input_path = cli.input_file;
+    let input_file = File::open(&input_path).map_err(|e| Error::FileOpenError(input_path.to_path_buf(), e))?;
+    let mut input_file = BufReader::new(input_file);
+
+    let rewrite_result = {
+        let output_file = output_file.as_write();
+        let mut output_file = BufWriter::new(output_file);
+        let rewrite = CommentHeaderRewrite::new(rewriter_config);
+        let abort_on_unchanged = true;
+        rewrite_stream(rewrite, &mut input_file, &mut output_file, abort_on_unchanged)
+    };
+    match rewrite_result {
+        Err(e) => {
+            eprintln!("Failure during processing of {}.", input_path.display());
+            return Err(e);
+        }
+        Ok(SubmitResult::Good) => {
+            // We finished processing the file but never got the headers
+            eprintln!("File {} appeared to be oddly truncated. Doing nothing.", input_path.display());
+        }
+        Ok(SubmitResult::HeadersUnchanged(comments)) => match operation_mode {
+            OperationMode::List => {
+                comments.write_as_text(io::stdout()).map_err(Error::ConsoleIoError)?;
+            }
+            _ => todo!("Headers unchanged for non-list operation"),
+        },
+        Ok(SubmitResult::HeadersChanged { .. }) => todo!("Headers changed"),
+    };
     Ok(())
 }
