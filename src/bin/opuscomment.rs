21429d6f5443c74751d05319ac5a308cb7eb7f34
Francis Russell <francis@unchartedbackwaters.co.uk>
2022-10-19 00:43:40 +0100

    Add parsing of tags to delete.

--- src/bin/opuscomment.rs
+++ src/bin/opuscomment.rs
@@ -1,15 +1,17 @@
 #[path = "../output_file.rs"]
 mod output_file;
 
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{self, BufReader, BufWriter};
+use std::ops::BitOrAssign;
 use std::path::PathBuf;
 
 use clap::Parser;
 use output_file::OutputFile;
 use zoog::comment_rewriter::{CommentHeaderRewrite, CommentRewriterAction, CommentRewriterConfig};
 use zoog::header_rewriter::{rewrite_stream, SubmitResult};
-use zoog::opus::{parse_comment, CommentList, DiscreteCommentList};
+use zoog::opus::{parse_comment, validate_comment_field_name, CommentList, DiscreteCommentList};
 use zoog::Error;
 
 fn main() {
@@ -37,9 +39,13 @@ struct Cli {
     /// Replace comments in the Ogg Opus file
     write: bool,
 
-    #[clap(short = 't', long = "tag", id = "TAG")]
+    #[clap(short = 't', long = "tag", id = "NAME")]
     /// Specify a tag
     tags: Vec<String>,
+
+    #[clap(short = 'd', long = "rm", id = "NAME[=VALUE]")]
+    /// Specify a tag name or name-value mapping to be deleted
+    delete: Vec<String>,
 
     /// Input file
     input_file: PathBuf,
@@ -55,13 +61,81 @@ enum OperationMode {
     Replace,
 }
 
-fn comments_to_list<S: AsRef<str>, I: IntoIterator<Item = S>>(comments: I) -> Result<DiscreteCommentList, Error> {
+/// Match type for Opus comment values
+#[derive(Clone, Debug)]
+enum ValueMatch {
+    All,
+    ContainedIn(HashSet<String>),
+}
+
+impl ValueMatch {
+    pub fn singleton(value: String) -> ValueMatch {
+        let mut set = HashSet::with_capacity(1);
+        set.insert(value);
+        ValueMatch::ContainedIn(set)
+    }
+
+    pub fn matches(&self, value: &str) -> bool {
+        match self {
+            ValueMatch::All => true,
+            ValueMatch::ContainedIn(values) => values.contains(value),
+        }
+    }
+}
+
+impl Default for ValueMatch {
+    fn default() -> ValueMatch { ValueMatch::ContainedIn(HashSet::new()) }
+}
+
+impl BitOrAssign for ValueMatch {
+    fn bitor_assign(&mut self, rhs: ValueMatch) {
+        let mut old_lhs = ValueMatch::All;
+        std::mem::swap(self, &mut old_lhs);
+        let new_set = match (old_lhs, rhs) {
+            (ValueMatch::ContainedIn(mut lhs), ValueMatch::ContainedIn(rhs)) => {
+                lhs.extend(rhs.into_iter());
+                Some(lhs)
+            }
+            _ => None,
+        };
+        if let Some(new_set) = new_set {
+            *self = ValueMatch::ContainedIn(new_set);
+        }
+    }
+}
+
+fn parse_new_comment_args<S: AsRef<str>, I: IntoIterator<Item = S>>(comments: I) -> Result<DiscreteCommentList, Error> {
     let comments = comments.into_iter();
     let mut result = DiscreteCommentList::with_capacity(comments.size_hint().0);
     for comment in comments {
         let comment = comment.as_ref();
         let (key, value) = parse_comment(comment)?;
         result.append(&key, &value)?;
+    }
+    Ok(result)
+}
+
+fn parse_delete_comment_args<S, I>(patterns: I) -> Result<HashMap<String, ValueMatch>, Error>
+where
+    S: AsRef<str>,
+    I: IntoIterator<Item = S>,
+{
+    let patterns = patterns.into_iter();
+    let mut result = HashMap::new();
+    for pattern_string in patterns {
+        let pattern_string = pattern_string.as_ref();
+        let (key, value) = match parse_comment(pattern_string) {
+            Ok((key, value)) => (key, Some(value)),
+            Err(_) => match validate_comment_field_name(pattern_string) {
+                Ok(()) => (pattern_string.to_string(), None),
+                Err(e) => return Err(e),
+            },
+        };
+        let rhs = match value {
+            None => ValueMatch::All,
+            Some(value) => ValueMatch::singleton(value),
+        };
+        *result.entry(key).or_default() |= rhs;
     }
     Ok(result)
 }
@@ -76,8 +150,11 @@ fn main_impl() -> Result<(), Error> {
         (true, true) => panic!("Append and replace cannot be specified at the same time"),
     };
 
-    let tags = comments_to_list(cli.tags)?;
-    println!("Operating in mode: {:?} (tags={:?})", operation_mode, tags);
+    let tags = parse_new_comment_args(cli.tags)?;
+    let delete_tags = parse_delete_comment_args(cli.delete)?;
+    println!("Operating in mode: {:?}", operation_mode);
+    println!("tags={:?}", tags);
+    println!("delete_tags={:?}", delete_tags);
 
     let action = match operation_mode {
         OperationMode::Inspect => CommentRewriterAction::NoChange,
